1aa33b453073ef2945560e881cb6cb32d8b47ecb
taevt-iscte <taevt@iscte-iul.pt>
2020-11-14 17:34:59 +0000

    Deck ask functionality partially implementes, needs courses

--- src/src/Main.scala
+++ src/src/Main.scala
@@ -1,8 +1,28 @@
 import About.about
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    println(about())
+import scala.annotation.tailrec
+import scala.io.StdIn.readLine
+import scala.sys.exit
 
+object Main {
+
+  def getUserInput: String = {
+    readLine.trim.toUpperCase
+  }
+
+  @tailrec
+  def mainLoop(): Unit = {
+    val input = getUserInput()
+    input match {
+    case "ABOUT" => print (about () );
+    case "ASK" =>
+    case "QUIT" => exit(0)
+    case _ =>
+  }
+    mainLoop()
+  }
+
+  def main(args: Array[String]): Unit = {
+    mainLoop()
   }
 }
