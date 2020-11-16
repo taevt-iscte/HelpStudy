89c9eaf8e06cd317677f0fb9cbb2ac8f95200ccb
jfrpo1-iscte <jfrpo1@iscte-iul.pt>
2020-11-16 19:31:44 +0000

    reminder 0.2

--- src/src/Main.scala
+++ src/src/Main.scala
@@ -1,8 +1,17 @@
+import java.time.LocalDate
+
 import About.about
+import RemindersManager.sort_default
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(about())
-
+    //println(about()) ----FUNCIONA
+    val rems: RemindersManager = RemindersManager(List(("Titulo1", "Body1", 3, LocalDate.now()),
+      ("Titulo2", "Body2", 1, LocalDate.now()), ("Titulo3", "Body3", 2, LocalDate.now())))
+    //println(rems.sort_by_priority())
+    //println(rems.addReminder(("Titulo4", "Body4", 4, LocalDate.now())))
+    //println(rems.delReminder("Titulo2"))
+    //println(sort_default(rems))
+    //println(rems.searchReminder("Titulo7"))
   }
 }
--- src/src/RemindersManager.scala
+++ src/src/RemindersManager.scala
@@ -1,20 +1,55 @@
+import java.time.LocalDate
 
-case class RemindersManager(lst_rem: List[Reminder]) {
+import RemindersManager.{Reminder, Reminder_List}
 
-  def sort_by_priority(): List[Reminder] = RemindersManager.sort_by_priority(this)
+case class RemindersManager(val lst_rem: Reminder_List) {
 
-  def sort_by_date(): List[Reminder] = RemindersManager.sort_by_date(this)
+  def addReminder(new_rem: Reminder): Reminder_List = RemindersManager.addReminder(this, new_rem)
+
+  def delReminder(title: String): Reminder_List = RemindersManager.delReminder(this, title)
+
+  def searchReminder(title: String): Option[Reminder] = RemindersManager.searchReminder(this, title)
+
+  def sort_by_priority(): Reminder_List = RemindersManager.sort_by_priority(this)
+
+  def sort_by_date(): Reminder_List = RemindersManager.sort_by_date(this)
 }
 
 object RemindersManager {
+  type Title = String
+  type Body = String
+  type Priority = Int
+  type Date = LocalDate
+  type Reminder = (Title, Body, Priority, Date)
+  type Reminder_List = List[Reminder]
 
 
-  def sort_by_priority(rem_man: RemindersManager): List[Reminder] = {
-    rem_man.lst_rem.sortBy(_.priority)
+  //-------------UPDATE------------------------
+  def addReminder(rem_man: RemindersManager, new_rem: Reminder): Reminder_List = {
+    new_rem :: rem_man.lst_rem
   }
 
-  def sort_by_date(rem_man: RemindersManager): List[Reminder] = {
-    rem_man.lst_rem.sortBy(_.date)
+  def delReminder(rem_man: RemindersManager, title: String): Reminder_List = {
+    rem_man.lst_rem.filter( r => !r._1.equals(title))
+  }
+
+
+  //------------AUXILIAR------------------------------
+  def searchReminder(rem_man: RemindersManager, title: String): Option[Reminder] = {
+    Option((rem_man.lst_rem filter ( r => r._1.equals(title))) (0))
+  }
+
+  //-------------SORTING------------------------------
+  def sort_by_priority(rem_man: RemindersManager): Reminder_List = {
+    rem_man.lst_rem.sortBy(_._3)
+  }
+
+  def sort_by_date(rem_man: RemindersManager): Reminder_List = {
+    rem_man.lst_rem.sortBy(_._4)
+  }
+
+  def sort_default(rem_man: RemindersManager): Reminder_List = {
+    rem_man.lst_rem.sortBy( r => (r._4, r._3))
   }
 
 }
