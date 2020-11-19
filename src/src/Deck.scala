4d8df08122b918623e875cf3f2331984ccaea703
taevt-iscte <taevt@iscte-iul.pt>
2020-11-19 11:48:33 +0000

    Basic SRS added, mastered cards won't show up again Added print cards filter

--- src/src/Deck.scala
+++ src/src/Deck.scala
@@ -2,18 +2,19 @@ import Deck.Card
 
 import scala.annotation.tailrec
 import scala.io.StdIn.readLine
+import java.time.LocalDate
 
 case class Deck(cards: List[Card]){
   def addCard(card: Card): (List[Card], Deck) = Deck.addCard(this, card)
 
   def ask(course: String): Unit = Deck.ask(this, course)
 
-  def printCards(cards: List[Card]): Unit = Deck.printCards(cards)
+  def printCards(cards: List[Card])(opt: String): Unit = Deck.printCards(cards)(opt)
 }
 
 object Deck {
 
-  type Card = (String, String, Int, String)
+  type Card = (String, String, Int, String, LocalDate)
 
   def addCard(deck: Deck, card: Card): (List[Card], Deck) = (card :: deck.cards, Deck(card::deck.cards))
 
@@ -21,15 +22,31 @@ object Deck {
     val card = deck.cards.filter(_._4 == course)(Math.random().toInt % deck.cards.size)
     print(card._1)
     val answer = readLine.trim
-    if (answer.compareToIgnoreCase(card._2) == 0) { println("Correct")
-     val index = deck.cards.indexOf(card)
-     Deck(deck.cards.updated(index, (card._1, card._2, card._3+1, card._4)))} else {println("Wrong"); deck}
+    val index = deck.cards.indexOf(card)
+    if (answer.compareToIgnoreCase(card._2) == 0) {
+      println("Correct")
+      val newDate = LocalDate.now().plusDays(deck.cards(index)._3 * 2)
+      if (deck.cards(index)._3 >= 5)
+        Deck(deck.cards.updated(index, (card._1, card._2, card._3+1, card._4, LocalDate.MAX)))
+      else
+        Deck(deck.cards.updated(index, (card._1, card._2, card._3+1, card._4, newDate)))
+    }
+    else {
+      println("Wrong")
+      Deck(deck.cards.updated(index, (card._1, card._2, 1, card._4, LocalDate.now())))
+    }
   }
 
-  @tailrec
-  def printCards(cards: List[Card]) : Unit = cards match {
+  def printCards(cards: List[Card])(opt: String = "") : Unit = {
+    @tailrec
+    def aux(card_list: List[Card]): Unit = cards match {
     case Nil => Nil
-    case card :: tail => println(s"Q: $card._1 A: $card._2"); printCards(tail)
+    case card :: tail => println(s"Q: ${card._1} A: ${card._2} Level: ${card._3} Next Due Date: ${card._5}")
+        aux(tail)
+  }
+    if (opt == "")
+      aux(cards)
+    else aux(cards.filter(card => card._4 == opt))
   }
 
 }
