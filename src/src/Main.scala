71b5d33b8502ab059743ef387341ce55eef809a7
lmsps-iscte <lmsps@iscte-iul.pt>
2020-11-20 23:10:04 +0000

    Main with schedule menu

--- src/src/Main.scala
+++ src/src/Main.scala
@@ -1,5 +1,5 @@
 import java.nio.file.Paths
-import java.time.LocalDate
+import java.time.{LocalDate, LocalTime}
 
 import About.about
 
@@ -28,12 +28,60 @@ object Main {
       case "DECK" => //deckLoop()
       case "NOTEBOOK" => //notebookLoop()
       case "REMINDERS" => remindersLoop()
-      case "SCHEDULE" => //scheduleLoop()
+      case "SCHEDULE" => scheduleLoop(Schedule(Nil, 50))
       case "SUBJECTS" => //subjectsLoop()
       case "QUIT" => exit(0)
       case _ =>
     }
     mainLoop()
+  }
+
+  def scheduleLoop(schedule: Schedule): Unit = {
+    print("\nESCOLHA UMA DAS SEGUINTES OPÇÕES:")
+    print("\nADD_BLOCK, BLOCKS_BY_DAY, TIME_CUNIT_L7DAYS, TIME_CUNIT_L30DAYS, " +
+      "TIME_SPENT_TODAY, FATIGUE_ALERT OU BACK")
+    print("\nINPUT: ")
+    val input = getUserInput
+    input match {
+      case "ADD_BLOCK" => {
+        print("\nINSIRA A DATA (EX:. 2020-11-20): ")
+        val date = LocalDate.parse(getUserInput)
+        print("\nINSIRA A HORA DE INÍCIO (EX:. 10:30): ")
+        val start_time = LocalTime.parse(getUserInput)
+        print("\nINSIRA A HORA DE FIM (EX:. 11:30): ")
+        val end_time = LocalTime.parse(getUserInput)
+        print("\nINSIRA O TÍTULO: ")
+        val title = getUserInput
+        print("\nINSIRA A UNIDADE CURRICULAR: ")
+        val cunit = getUserInput
+        scheduleLoop(schedule.addSBlock(SBlock(date,start_time,end_time,title,cunit)))
+      }
+      case "BLOCKS_BY_DAY" => {
+        print("\nINSIRA A DATA (EX:. 2020-11-20): ")
+        val date = LocalDate.parse(getUserInput)
+        schedule.blocksByDay(date)
+      }
+      case "TIME_CUNIT_L7DAYS" => {
+        print("\nINSIRA A UNIDADE CURRICULAR: ")
+        val cunit = getUserInput
+        print("\nNOS ÚLTIMOS 7 DIAS INVESTIU "+schedule.timebyCUnitL7Days(cunit)+" MINUTOS")
+      }
+      case "TIME_CUNIT_L30DAYS" => {
+        print("\nINSIRA A UNIDADE CURRICULAR: ")
+        val cunit = getUserInput
+        print("\nNOS ÚLTIMOS 30 DIAS INVESTIU "+schedule.timebyCUnitL30Days(cunit)+" MINUTOS")
+      }
+      case "TIME_SPENT_TODAY" => {
+        print("\nHOJE VAI INVESTIR "+schedule.timeSpentBySchoolToday()+" MINUTOS DO SEU DIA")
+      }
+      case "FATIGUE_ALERT" => {
+        print("\n"+schedule.fatigueAlert())
+      }
+      case "BACK" => mainLoop()
+      case "QUIT" => exit(0)
+      case _ =>
+    }
+    scheduleLoop(schedule)
   }
 
   def remindersLoop(): Unit = {
